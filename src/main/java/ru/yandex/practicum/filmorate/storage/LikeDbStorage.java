c39c0b24a698708c015ea5f1dbd4455ef7769029
Максим Брейлян <maxim.breilyan@yandex.ru>
2024-08-13 01:01:33 +0300

    финальное задание спринт 12

--- src/main/java/ru/yandex/practicum/filmorate/storage/LikeDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/LikeDbStorage.java
@@ -11,10 +11,10 @@ import java.util.List;
 public class LikeDbStorage {
     private final JdbcTemplate jdbc;
 
-    private final static String SELECT_ALL_LIKES = "select film_id FROM likes group by film_id order by count(user_id) DESC";
-    private final static String INSERT_LIKES = "INSERT INTO likes(film_id, user_id) VALUES(?,?)";
-    private final static String DELETE_LIKES_BY_FILM_ID = "DELETE FROM likes WHERE film_id=? AND user_id=?";
-    private final static String SELECT_LIKES_BY_FILM_ID = "SELECT user_id FROM likes WHERE film_id=?";
+    private static final String SELECT_ALL_LIKES = "select film_id FROM likes group by film_id order by count(user_id) DESC";
+    private static final String INSERT_LIKES = "INSERT INTO likes(film_id, user_id) VALUES(?,?)";
+    private static final String DELETE_LIKES_BY_FILM_ID = "DELETE FROM likes WHERE film_id=? AND user_id=?";
+    private static final String SELECT_LIKES_BY_FILM_ID = "SELECT user_id FROM likes WHERE film_id=?";
 
     public List<Integer> getFilmsIdByLike() {
         return jdbc.queryForList(SELECT_ALL_LIKES, Integer.class);
